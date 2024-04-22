57310303b753d20b6c1be1eb14d5f1eae71b8e5e
Rudbeckiya <113553747+Rudbeckiya@users.noreply.github.com>
2024-04-22 13:43:05 +0300

    Added log

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,51 @@
 from flask import Flask
 from flask import request
 import math
+from datetime import datetime, timezone, timedelta
 
 app = Flask(__name__)
 
+log = list()
+
 @app.route('/')
 def readme():
-    return 'Api имеет следующий функционал:<br><br>Для операций с двумя числами (в кавычках): "/действие/число1/число2?mode=int_или_float", где вместо "действие" может быть "add" (сложение), "sub" (вычитание), "mul" (умножение), "div" (деление), "pow" (возведение в степень), "mod" (остаток от деления).<br><br>Для извлечения квадратного корня: "/sqrt/число?mode=int_или_float".<br><br>Для тригонометрических функций: "/действие/число?mode=int_или_float&rad=yes_или_no", где вместо "действие" может быть "sin" (синус), "cos" (косинус), "tan" (тангенс). '
+    log_append()
+
+    url = '''Api имеет следующий функционал:<br><br>
+    Для операций с двумя числами (в кавычках): "/действие/число1/число2?mode=int_или_float",
+    где вместо "действие" может быть "add" (сложение), "sub" (вычитание), "mul" (умножение), "div" (деление), "pow" (возведение в степень), "mod" (остаток от деления).<br><br>
+    Для извлечения квадратного корня: "/sqrt/число?mode=int_или_float".<br><br>
+    Для тригонометрических функций: "/действие/число?mode=int_или_float&rad=yes_или_no", где вместо "действие" может быть "sin" (синус), "cos" (косинус), "tan" (тангенс).<br><br>
+    Для очистки лога: "/log_clear".<br><br>
+    Лог запросов:<br>'''
+
+    for i in log:
+        url += i + "<br>"
+    return url
+
+def log_append():
+    timezone_offset = +3.0
+    tzinfo = timezone(timedelta(hours=timezone_offset))
+
+    if (request.base_url != "http://rudbeckiya.pythonanywhere.com/"):
+        if (len(log) == 1):
+            log.append(str(datetime.now(tzinfo).strftime("[%Y.%m.%d | %H:%M:%S]")) + " " + str(request.base_url))
+            log.reverse()
+        else:
+            if (len(log) != 25):
+                log.reverse()
+                log.append(str(datetime.now(tzinfo).strftime("[%Y.%m.%d | %H:%M:%S]")) + " " + str(request.base_url))
+                log.reverse()
+            else:
+                log.pop(24)
+                log.reverse()
+                log.append(str(datetime.now(tzinfo).strftime("[%Y.%m.%d | %H:%M:%S]")) + " " + str(request.base_url))
+                log.reverse()
+
+@app.route('/log_clear')
+def log_clear():
+    log.clear()
+    return 'Лог успешно очищен.'
 
 def convert_int(a, b):
     return [int(a), int(b)]
@@ -19,6 +58,8 @@ def convert_radian(a):
 
 @app.route('/add/<a>/<b>')
 def add(a, b):
+    log_append()
+
     format = request.args.get('mode')
     if format == 'int':
         a, b = convert_int(a, b)
@@ -31,6 +72,8 @@ def add(a, b):
 
 @app.route('/sub/<a>/<b>')
 def sub(a, b):
+    log_append()
+
     format = request.args.get('mode')
     if format == 'int':
         a, b = convert_int(a, b)
@@ -43,6 +86,8 @@ def sub(a, b):
 
 @app.route('/mul/<a>/<b>')
 def mul(a, b):
+    log_append()
+
     format = request.args.get('mode')
     if format == 'int':
         a, b = convert_int(a, b)
@@ -55,6 +100,8 @@ def mul(a, b):
 
 @app.route('/div/<a>/<b>')
 def div(a, b):
+    log_append()
+
     format = request.args.get('mode')
     if format == 'int':
         a, b = convert_int(a, b)
@@ -70,6 +117,8 @@ def div(a, b):
 
 @app.route('/sqrt/<a>')
 def sqrt(a):
+    log_append()
+
     if float(a) < 0:
         return "Number must be >= 0"
 
@@ -83,6 +132,8 @@ def sqrt(a):
 
 @app.route('/mod/<a>/<b>')
 def mod(a, b):
+    log_append()
+
     format = request.args.get('mode')
     if format == 'int':
         a, b = convert_int(a, b)
@@ -98,6 +149,8 @@ def mod(a, b):
 
 @app.route('/pow/<a>/<b>')
 def pow(a, b):
+    log_append()
+
     format = request.args.get('mode')
     if format == 'int':
         a, b = convert_int(a, b)
@@ -110,6 +163,8 @@ def pow(a, b):
 
 @app.route('/sin/<a>')
 def sin(a):
+    log_append()
+
     format = request.args.get('mode')
     if format == 'int':
         a = int(a)
@@ -128,6 +183,8 @@ def sin(a):
 
 @app.route('/cos/<a>')
 def cos(a):
+    log_append()
+
     format = request.args.get('mode')
     if format == 'int':
         a = int(a)
@@ -146,6 +203,8 @@ def cos(a):
 
 @app.route('/tan/<a>')
 def tan(a):
+    log_append()
+
     format = request.args.get('mode')
     if format == 'int':
         a = int(a)
